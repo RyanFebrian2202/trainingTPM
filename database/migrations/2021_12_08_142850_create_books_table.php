2b7ed7404292b4a3a4b656bb71f40f6fde3ccb7e
RyanFebrian2202 <ryan22febrian@gmail.com>
2021-12-11 22:01:35 +0700

    added training tpm week 1

---
+++ database/migrations/2021_12_08_142850_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //id, title, author, release, prize 
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->date('release');
+            // $table->date('release')->nullable();
+            //nullable artinya blh null
+            // $table->string('title')->unique();
+            //unique tuh gk blh ada yg sama jd kalo udh moon gbs moon lg
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
---
+++ resources/views/create.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Create Form</h1>
+
+    {{-- enctype itu hrs ada utk setiap form bila menggunakan laravel --}}
+    <form action="{{ route('createBook') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        {{-- krn gk ada method delete maka bisa pake "@method("DELETE")" --}}
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input name="title" type="text" class="form-control" id="formGroupExampleInput"
+                placeholder="Example input placeholder">
+        </div>
+        <div class="mb-3">
+            <label for="author" class="form-label">Author</label>
+            <input name="author" type="text" class="form-control" id="formGroupExampleInput"
+                placeholder="Example input placeholder">
+        </div>
+        <div class="mb-3">
+            <label for="release" class="form-label">Release Date</label>
+            <input name="release" type="date" class="form-control" id="formGroupExampleInput"
+                placeholder="Example input placeholder">
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Price</label>
+            <input name="price" type="numeric" class="form-control" id="formGroupExampleInput"
+                placeholder="Example input placeholder">
+        </div>
+        <button type="submit" class="btn btn-success">Insert</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
